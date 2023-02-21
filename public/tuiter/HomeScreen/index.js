4a569b2decb4c2da148da8e48fe9053e68fd587a
ankitsaxena35 <ankitsaxena35@yahoo.in>
2023-02-20 22:02:16 -0500

    bug fixes

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -2,7 +2,7 @@ import NavigationSidebar from "../NavigationSidebar/index.js";
 import PostItems from "../PostList/index.js";
 import PostSummaryList from "../PostSummaryList/index.js";
 
-(function ($) {
+function HomeScreenComponent() {
     $('#wd-home').append(`
         <div class="row">
         <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-2 col-xxl-2 mt-2 wd-border-right">
@@ -16,4 +16,5 @@ import PostSummaryList from "../PostSummaryList/index.js";
             </div>
         </div>
     `);
-})($);
+}
+$(HomeScreenComponent);
