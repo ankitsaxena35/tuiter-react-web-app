4feb1cf9343a9132a7bbc6a8441f743152c5f61e
ankitsaxena35 <ankitsaxena35@yahoo.in>
2023-02-20 21:54:21 -0500

    bug fixes

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -1,6 +1,6 @@
-import NavigationSidebar from "../NavigationSidebar";
-import PostItems from "../PostList";
-import PostSummaryList from "../PostSummaryList";
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import PostItems from "../PostList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
 
 (function ($) {
     $('#wd-home').append(`
